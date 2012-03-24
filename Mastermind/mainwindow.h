51a4a32b9154ee13f0c9156259090a42cc7ebe22
pierok <pierok@gmail.com>
2012-03-24 12:43:04 +0100

    dupa 2

--- Mastermind/mainwindow.h
+++ Mastermind/mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -37,41 +36,3 @@ private slots:
 };
 
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QtGui>
-#include "plansza.h"
-#include "populacja.h"
-#include "algorytmgenetyczny.h"
-
-namespace Ui {
-    class MainWindow;
-}
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-    QTimer maintimer;
-    Plansza *plansza;
-    AlgorytmGenetyczny *algorytm;
-    Osobnik* osobnik;
-    bool start;
-
-private slots:
-    void MainClockTick();
-    void on_acceptButton_clicked();
-    void on_newGameButton_clicked();
-};
-
-#endif // MAINWINDOW_H
->>>>>>> 71f1fe1a7f0e7da241082634dfd6908420743548
--- Mastermind/selectframe.h
+++ Mastermind/selectframe.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef SELECTFRAME_H
 #define SELECTFRAME_H
 
@@ -39,46 +38,3 @@ protected:
 };
 
 #endif // SELECTFRAME_H
-=======
-#ifndef SELECTFRAME_H
-#define SELECTFRAME_H
-
-#include <QGraphicsItem>
-#include <QGraphicsScene>
-#include <QPainter>
-#include <QStyleOption>
-#include "elipseframe.h"
-
-class SelectFrame : public QGraphicsItem
-{
-public:
-
-    SelectFrame();
-    ~SelectFrame()
-    {
-        for(int i=0; i<8; ++i)
-        {
-            delete memory[i];
-        }
-    }
-
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-               QWidget *widget);
-
-    QRectF  boundingRect() const;
-    void    setBoundingRect(QRectF);
-
-    ElipseFrame* memory[8];
-    void clearMemory();
-    void frameClicked(double x, double y);
-    int level;
-
-protected:
-
-    QRectF m_boundingRect;
-
-
-};
-
-#endif // SELECTFRAME_H
->>>>>>> 71f1fe1a7f0e7da241082634dfd6908420743548
